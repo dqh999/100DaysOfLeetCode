54dc4a8f07d99dac7e4552fe6bd29cd092c85565
dqh999 <dqhit999@gmail.com>
2025-01-11 15:34:39 +0700

    2. add two numbers

--- 2. Add Two Numbers/src/Solution.java
+++ 2. Add Two Numbers/src/Solution.java
@@ -1,28 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private Map<String, Integer> addTwoNumbers(
-            int number1,
-            int number2
-    ){
-        Map<String, Integer> result = new HashMap<>();
-        result.put("carry", (number1 + number2) % 10);
-        result.put("result", number1 + number2);
-        return result;
-    };
-    public ListNode addTwoNumbers(
-            ListNode l1,
-            ListNode l2
-    ) {
-        ListNode dummyHead  = new ListNode(0);
-        var result1 = addTwoNumbers(l1.val, l2.val);
-        int carry = result1.get("carry");
-        dummyHead.val = result1.get("result");
-        while (l1.next != null && l2.next != null) {
-            var resultNext = addTwoNumbers(l1.next.val, l2.next.val);
-
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            sum %= 10;
+            current.next = new ListNode(sum);
+            current = current.next;
         }
-        return dummyHead.next;
+        return result.next;
     }
 }
---
+++ 2. Add Two Numbers/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    private int val;
+    public Test(int val) {
+        this.val = val;
+    }
+    public void set(int val) {
+        this.val = val;
+    }
+    public int get() {
+        return val;
+    }
+}
